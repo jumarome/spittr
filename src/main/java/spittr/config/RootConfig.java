6cb3d72fb17daa87fb37714a05e44a7c9f588cec
Juan <jmanuelromero93@gmail.com>
2017-02-26 18:16:37 -0500

    Configurando el datasource para H2 y los scripts de creacion y datos

--- src/main/java/spittr/config/RootConfig.java
+++ src/main/java/spittr/config/RootConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -11,10 +12,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * @author Juan Manuel Romero
  */
 @Configuration
+@Import(DataConfig.class)
 @ComponentScan(basePackages = {"spittr"},
         excludeFilters = {
             @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
         })
 public class RootConfig {
 
+    
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+/**
+ * Author:  Juan Manuel Romero
+ * Created: 26/02/2017
+ */
+
+INSERT  INTO  SPITTLE  VALUES ( 1,'El inicio de una aventura ha comenzado..','2017-02-26 02:00:17',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 2,'Disfrutando de un bonito feriado con la familia','2017-02-26 02:01:17',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 3,'Habra segunda vuelta #entucaracorrea','2017-02-26 02:03:27',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 4,'Hoy juega el equipo de mis amores Emelec','2017-03-03 14:30:17',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 5,'Se prendio esta mielda...','2017-03-03 16:00:05',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 6,'Y nos fuimos de parrilada con la familia..','2017-03-05 17:00:58',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 7,'Buen inicio de semana para todos..','2017-03-06 00:00:10',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 8,'Muy cerca de un gran objetivo...','2017-03-23 04:09:47',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 9,'Feliz Cumpleaños @German','2017-04-23 16:29:37',0.0,0.0);
+INSERT  INTO  SPITTLE  VALUES ( 10,'Hasta pronto amigos mios','2017-04-26 18:10:57',0.0,0.0);
